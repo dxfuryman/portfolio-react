ec4bc2acc5939c2f3d940259e966c2cdc725a21a
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2024-04-09 14:41:25 +0530

    dev commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import ProfileImage from "./assets/profile.jpg";
 
 const Home = () => {
     return (
         <>
             <div className="home-container">
-                <h1 className="home-title">YES!! I am a <span id="FD">Frontend Developer</span></h1>
+                <h1 className="home-title"><span className="yes"> YES!!</span> I am a <span id="FD">Frontend Developer</span></h1>
             </div>
         </>
     )
