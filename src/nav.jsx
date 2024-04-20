bf8727b2dcfaef6e07f47ff5d9dd7c065cebcd51
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2024-04-20 10:56:34 +0530

    dev commit

--- src/nav.jsx
+++ src/nav.jsx
@@ -2,36 +2,20 @@ import React from "react";
 import './nav.css'; // Keep your CSS import
 import { NavLink } from 'react-router-dom'; // Import NavLink
 import { Link } from 'react-scroll';
-import {useState} from "react";
 
 
 function Navbar() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
-
     return (
-        <>
+        <p>
             <span id="name">Vineet Somani</span>
-
-            {/* Hamburger Button */}
-            <div className="hamburger-menu" onClick={toggleMenu}>
-                <div className={isMenuOpen ? "line line-1-open" : "line line-1"}></div>
-                <div className={isMenuOpen ? "line line-2-open" : "line line-2"}></div>
-                <div className={isMenuOpen ? "line line-3-open" : "line line-3"}></div>
-            </div>
-        </>
+        </p>
     );
 }
 
 function Navbar2() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
         <div className="btn-nav">
-            {/* Main Navigation (Hidden when menu is closed) */}
-            <div className={`btn-nav-main ${isMenuOpen ? 'open' : ''}`}>
+            <div className="btn-nav-main">
                 <nav>
                     <NavLink to="/" activeClassName="active"><button className={"btn"}><h2>Home</h2></button></NavLink>
                     <NavLink to="/about" activeClassName="active"><button className={"btn"}><h2>About</h2></button></NavLink>
@@ -40,7 +24,7 @@ function Navbar2() {
             </div>
             <nav>
                 <div className="special">
-                    {/* Hire Me Button */}
+                    <NavLink to="/hire" activeClassName="active"><button className="btn2"><h2>Hire me </h2></button></NavLink>
                 </div>
             </nav>
         </div>
