04decfd9bd1e9f400a17b67ffe0b12532434bed4
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2024-04-26 16:38:53 +0530

    dev commit

--- src/projects/Portfolio.jsx
+++ src/projects/Portfolio.jsx
@@ -14,42 +14,42 @@ export const Portfolio = () => {
             <div className="main">
                 <h1 id="ttl">My Projects</h1>
                 <div className="pj-wrap">
-                   <button id={"btn-1"}>
+                   <button className={"btn-1"}>
                        <a href="https://react-router-five-orpin.vercel.app/" target={"_blank"}><img src={prj5}/></a>
                    <label>Community Website</label>
                    </button>
-                    <button id={"btn-2"}>
+                    <button className={"btn-1"}>
                         <a href={"https://vikhyatsingh123.github.io/Naruto-Shippuden/"} target={"_blank"}>
                             <img src={prj2}/>
                         </a>
                         <label>Anime Fansite</label>
 
                     </button>
-                    <button id={"btn-3"}>
+                    <button className={"btn-1"}>
                         <a href={"https://pass-word-generator-eight.vercel.app/"} target={"_blank"}>
                             <img src={prj3}/>
                         </a>
                         <label>Password Generator</label>
                     </button>
-                    <button id={"btn-4"}>
+                    <button className={"btn-1"}>
                         <a href={"https://dxfuryman.github.io/new-portfolio2/"} target={"_blank"}>
                             <img src={prj1}/>
                         </a>
                         <label>Personal Portfolio (OlD)</label>
                     </button>
-                    <button id={"btn-1"}>
+                    <button className={"btn-1"}>
                         <a href="https://currency-converter-rho-one.vercel.app/" target={"_blank"}><img src={prj4}/></a>
                         <label>Currency Converter</label>
                     </button>
-                    <button id={"btn-1"}>
+                    <button className={"btn-1"}>
                         <a href="https://dxfuryman.github.io/simon-game/" target={"_blank"}><img src={prj6}/></a>
                         <label>Simon Game</label>
                     </button>
-                    <button id={"btn-1"}>
+                    <button className={"btn-1"}>
                         <a href="https://dxfuryman.github.io/dog-meeting-site/" target={"_blank"}><img src={prj7}/></a>
                         <label>Dog Pet Shop</label>
                     </button>
-                    <button id={"btn-1"}>
+                    <button className={"btn-1"}>
                         <a href="https://investment-calculator-gilt.vercel.app/" target={"_blank"}><img src={prj8}/></a>
                         <label>Investment Calculator</label>
                     </button>
